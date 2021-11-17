6d1dce4362ea194f2d0a33f9d7af848af1c50f47
Isabela Nascimento <iaman.isabela@gmail.com>
2021-11-17 17:52:48 -0300

    Hello, World

--- wallet/urls.py
+++ wallet/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/', views.hello_world, name='hello_world')
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 def index(request):
     return HttpResponse('<h1>Hello, World<h1>')
+
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    obj = {'message': "got some data!", 'data': request.data}
+    if request.method == 'POST':
+        return Response({'message': obj['message'], 'data': obj['data']})
+    return Response({'message': 'Hello again, World\'s!'})
