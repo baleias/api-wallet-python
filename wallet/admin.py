7f92a4b5c906f09b5519bfe70d8871d1d6a91cfd
Isabela Nascimento <iaman.isabela@gmail.com>
2021-11-19 22:22:50 -0300

    creating admin settings

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, OrderItem, Product, Order, Cashback
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(Cashback)
