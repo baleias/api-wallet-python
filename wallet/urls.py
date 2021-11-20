03c718fdbf5717f485fd1091b5d3b98bc8eef34a
Isabela Nascimento <iaman.isabela@gmail.com>
2021-11-19 22:21:01 -0300

    creating URLs

--- wallet/urls.py
+++ wallet/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/', views.hello_world, name='hello_world')
+    path('customer/', views.CustomerList.as_view(), name='Customer'),
+    path('product/', views.OrderList.as_view(), name='Order'),
+    path('cashback/', views.CashbackList.as_view(), name='Cashback'),
 ]
