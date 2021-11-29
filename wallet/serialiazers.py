f6b238e908a2d180135ebef950c876b477f8e77a
Isabela Nascimento <iaman.isabela@gmail.com>
2021-11-29 15:11:19 -0300

    adjusting serializers

--- wallet/serialiazers.py
+++ wallet/serialiazers.py
@@ -1,11 +1,16 @@
 from .models import Customer, Order, Cashback
 from rest_framework import serializers
+from wallet.validators import *
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
-
+    def validate(self, data):
+        if not valid_cpf(data['cpf']):
+            raise serializers.ValidationError({'cpf' : "CPF is invalid"})
+        return data
+    
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
